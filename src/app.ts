239900d57a6d53009cdbd3d5a877c49d395b4b91
Erlend <49862976+Erb3@users.noreply.github.com>
2023-06-26 12:17:55 +0000

    feat: Create basic ssh server

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,30 @@
 // CC-SSH
 // Mozilla public license 2.0 - Erb3 & Pixium
+
+import { readFileSync } from "fs";
+import { Server as SSHServer} from "ssh2";
+
+const sshServer = new SSHServer({
+    hostKeys: [
+    ],
+    
+    banner: "CC-SSH by Pixium",
+    ident: "CC-SSH-by-Pixium"
+}, (client) => {
+    console.log(`Client connected.`);
+
+    client.on("authentication", (ctx) => {
+        ctx.accept();
+    }).on("ready", () => {
+        client.once("session", (accept, reject) => {
+            accept().once("pty", (accept: any, reject: any, info: any) => {
+                accept && accept();
+            }).once("shell", (accept:any, reject:any) => {
+                accept();
+            });
+        });
+    })
+
+}).listen(8009, "0.0.0.0", () => {
+    console.log("SSH server running!")
+});
